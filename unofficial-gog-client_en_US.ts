a0fbbf4296f6eff9637525f59a885dd218e5fc8f
Alexander Permyakov <pamugk@gmail.com>
2023-02-18 19:06:08 +0500

    UI
    - set up main application window layout

--- unofficial-gog-client_en_US.ts
+++ unofficial-gog-client_en_US.ts
@@ -1,3 +1,91 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"></TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="303"/>
+        <source>GOG Client (Unofficial)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="206"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="309"/>
+        <source>GOG GALAXY</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="213"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="310"/>
+        <source>Discover</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="233"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="311"/>
+        <source>Recent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="247"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="312"/>
+        <source>DEALS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="254"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="313"/>
+        <source>Store</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="268"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="314"/>
+        <source>All games</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="282"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="315"/>
+        <source>Wishlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="296"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="316"/>
+        <source>Orders history</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="310"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="317"/>
+        <source>GAMES</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="317"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="318"/>
+        <source>Owned games</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="331"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="319"/>
+        <source>Installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="345"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="320"/>
+        <source>FRIENDS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="352"/>
+        <location filename="build/Desktop/Debug/unofficial-gog-client_autogen/include/ui_mainwindow.h" line="321"/>
+        <source>Friends online</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
