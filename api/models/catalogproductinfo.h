2237282fa10aa2e4d7a176a1dc1f908e3016ff81
Alexander Permyakov <pamugk@gmail.com>
2023-02-27 01:18:28 +0500

    API
    - defined full catalog product info structure

---
+++ api/models/catalogproductinfo.h
@@ -0,0 +1,145 @@
+#ifndef CATALOGPRODUCTINFO_H
+#define CATALOGPRODUCTINFO_H
+
+#include <QtGlobal>
+
+#include <QDateTime>
+#include <QMap>
+#include <QString>
+#include <QVector>
+
+#include "./metatag.h"
+
+namespace api
+{
+    struct Bonus
+    {
+        quint64 id;
+        QString name;
+        MetaTag type;
+    };
+
+    struct Edition
+    {
+        quint64 id;
+        QString name;
+        bool hasProductCard;
+        QVector<Bonus> bonuses;
+    };
+
+    struct ContentRating
+    {
+        quint8 ageRating;
+        QString category;
+        QVector<QString> contentDescriptors;
+    };
+
+    struct Requirement
+    {
+        QString id;
+        QString name;
+        QString description;
+    };
+
+    struct SystemRequirements
+    {
+        QString type;
+        QString description;
+        QVector<Requirement> requirements;
+    };
+
+    struct SupportedOperatingSystem
+    {
+        QString name;
+        QString versions;
+        QVector<SystemRequirements> systemRequirements;
+    };
+
+    struct Video
+    {
+        QString provider;
+        QString videoId;
+        QString thumbnailId;
+        QString videoLink;
+        QString thumbnailLink;
+    };
+
+    struct Localization
+    {
+        struct
+        {
+            QString code;
+            QString name;
+        } language;
+        bool localizedText;
+        bool localizedAuio;
+    };
+
+    struct FormattedLink
+    {
+        QString href;
+        bool templated;
+        QVector<QString> formatters;
+    };
+
+    struct Series
+    {
+        quint64 id;
+        QString name;
+    };
+
+    struct GetCatalogProductInfoResponse
+    {
+        // General info
+        bool inDevelopment;
+        bool usingDosBox;
+        QString copyrights;
+        QString releaseStatus;
+        QString description;
+        QString overview;
+        QString featuresDescription;
+        QString additionalRequirements;
+
+        // Links
+        QString storeLink;
+        QString supportLink;
+        QString forumLink;
+        QString iconSquareLink;
+        QString logoLink;
+        QString boxArtImageLink;
+        QString backgroundImageLink;
+        QString galaxyBackgroundImageLink;
+        QVector<QString> includedGames;
+
+        // Main product info
+        bool availableForSale;
+        bool visibleInCatalog;
+        bool preorder;
+        bool visibleInAccount;
+        quint64 id;
+        QString title;
+        bool installable;
+        QDateTime globalReleaseDate;
+        bool hasProductCard;
+        QDateTime gogReleaseDate;
+        bool secret;
+        FormattedLink imageLink;
+        QString checkoutLink;
+        QString pricesLink;
+        QString productType;
+        QVector<Localization> localizations;
+        QVector<Video> videos;
+        QVector<Bonus> bonuses;
+        QVector<FormattedLink> screenshots;
+        QString publisher;
+        QVector<QString> developers;
+        QVector<SupportedOperatingSystem> supportedOperatingSystems;
+        QVector<HierarchicalMetaTag> tags;
+        QVector<MetaTag> properties;
+        QMap<QString, ContentRating> ratings;
+        QVector<MetaTag> fetures;
+        Series series;
+    };
+}
+
+#endif // CATALOGPRODUCTINFO_H
