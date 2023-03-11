e7d61146dbb5ee5db066192a1ee8e7f12af10e1e
Alexander Permyakov <pamugk@gmail.com>
2023-03-11 14:18:39 +0500

    UI
    - activated app background mode support

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setQuitOnLastWindowClosed(false);
 
     QCoreApplication::setOrganizationName("pamugk.github.com");
     QCoreApplication::setApplicationName("Nebulae");
