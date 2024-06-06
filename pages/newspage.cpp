2a8df6b6ae10f78c9908b2657b67856960af5d53
Alexander Permyakov <pamugk@gmail.com>
2024-06-06 21:33:37 +0500

    UI
    - implemented basic navigation between news items

--- pages/newspage.cpp
+++ pages/newspage.cpp
@@ -1,9 +1,10 @@
 #include "newspage.h"
 #include "ui_newspage.h"
 
- #include <QDesktopServices>
+#include <QDesktopServices>
 #include <QJsonDocument>
 #include <QNetworkReply>
+#include <QStyle>
 
 #include "../api/utils/newsserialization.h"
 #include "../widgets/clickablelabel.h"
@@ -76,9 +77,9 @@ void NewsPage::initialize(const QVariant &data)
                 auto headlineItemTitleLabel = new ClickableLabel(headlineItem);
                 headlineItemTitleLabel->setCursor(Qt::PointingHandCursor);
                 connect(headlineItemTitleLabel, &ClickableLabel::clicked,
-                        this, [this]()
+                        this, [this, id = item.id]()
                 {
-                    // TODO: navigate to clicked item
+                    emit navigate({Page::NEWS, id});
                 });
                 headlineItemTitleLabel->setText(QString("%1 (%2)").arg(item.title).arg(item.commentsCount));
                 headlineItemTitleLabel->setWordWrap(true);
@@ -103,7 +104,7 @@ void NewsPage::initialize(const QVariant &data)
     });
     newsPageReply = apiClient->getNews(0, systemLanguage, 11);
     connect(newsPageReply, &QNetworkReply::finished,
-            this, [this]()
+            this, [this, id = data.toULongLong()]()
     {
         auto networkReply = newsPageReply;
         newsPageReply = nullptr;
@@ -114,47 +115,96 @@ void NewsPage::initialize(const QVariant &data)
             api::GetNewsResponse data;
             parseNewsResponse(resultJson, data);
 
-            api::NewsItem displayedNews = data.items[0];
-            ui->titleLabel->setText(displayedNews.title);
-            bannerReply = apiClient->getAnything(displayedNews.imageLarge);
-            connect(bannerReply, &QNetworkReply::finished,
-                    this, [this]()
+            for (std::size_t i = 0; i < data.items.count(); i++)
             {
-                auto networkReply = bannerReply;
-                bannerReply = nullptr;
-
-                if (networkReply->error() == QNetworkReply::NoError)
+                if (data.items[i].id == id)
                 {
-                    QPixmap bannerImage;
-                    bannerImage.loadFromData(networkReply->readAll());
+                    const api::NewsItem &displayedNews = data.items[i];
+                    ui->titleLabel->setText(displayedNews.title);
+                    bannerReply = apiClient->getAnything(displayedNews.imageLarge);
+                    connect(bannerReply, &QNetworkReply::finished,
+                            this, [this]()
+                    {
+                        auto networkReply = bannerReply;
+                        bannerReply = nullptr;
 
-                    QSize imageSize = bannerImage.size() / 2;
-                    ui->coverLabel->setMinimumSize(imageSize);
-                    ui->coverLabel->setMaximumSize(imageSize);
-                    ui->coverLabel->setPixmap(bannerImage.scaled(imageSize, Qt::KeepAspectRatioByExpanding));
+                        if (networkReply->error() == QNetworkReply::NoError)
+                        {
+                            QPixmap bannerImage;
+                            bannerImage.loadFromData(networkReply->readAll());
+
+                            QSize imageSize = bannerImage.size() / 2;
+                            ui->coverLabel->setMinimumSize(imageSize);
+                            ui->coverLabel->setMaximumSize(imageSize);
+                            ui->coverLabel->setPixmap(bannerImage.scaled(imageSize, Qt::KeepAspectRatioByExpanding));
+                        }
+                        else if (networkReply->error() != QNetworkReply::OperationCanceledError)
+                        {
+                            qDebug() << networkReply->error()
+                                     << networkReply->errorString()
+                                     << QString(networkReply->readAll()).toUtf8();
+                        }
+                        networkReply->deleteLater();
+                    });
+
+                    ui->publishedLabel->setText(QLocale::system().toString(displayedNews.publishDate, QLocale::ShortFormat));
+                    ui->descriptionWebEngineView->setHtml(displayedNews.body);
+                    ui->seeCommentsButton->setText(ui->seeCommentsButton->text().arg(displayedNews.commentsCount));
+
+                    QUrl forumLink(displayedNews.forumThreadLink);
+                    connect(ui->seeCommentsButton, &QPushButton::clicked,
+                            this, [this, forumLink]()
+                    {
+                        QDesktopServices::openUrl(forumLink);
+                    });
+
+                    auto previousItemLabel = new ClickableLabel(ui->contentResultsScrollAreaContents);
+                    previousItemLabel->setFixedHeight(58);
+                    previousItemLabel->setWordWrap(true);
+                    previousItemLabel->setAlignment(Qt::AlignTop | Qt::AlignRight);
+                    previousItemLabel->setStyleSheet("padding-top: 20px; padding-right: 36px; padding-bottom: 20px; padding-left: 36px;");
+                    if (i < data.items.count() - 1)
+                    {
+                        previousItemLabel->setCursor(Qt::PointingHandCursor);
+                        const api::NewsItem &previousItem = data.items[i + 1];
+                        previousItemLabel->setText(previousItem.title);
+                        connect(previousItemLabel, &ClickableLabel::clicked,
+                                this, [this, previousId = previousItem.id]()
+                        {
+                            emit navigate({Page::NEWS, previousId});
+                        });
+                    } else
+                    {
+                        previousItemLabel->setEnabled(false);
+                    }
+                    ui->navigationLayout->addWidget(previousItemLabel);
+                    auto nextItemLabel = new ClickableLabel(ui->contentResultsScrollAreaContents);
+                    nextItemLabel->setFixedHeight(58);
+                    nextItemLabel->setWordWrap(true);
+                    nextItemLabel->setAlignment(Qt::AlignTop | Qt::AlignLeft);
+                    nextItemLabel->setStyleSheet("padding-top: 20px; padding-right: 36px; padding-bottom: 20px; padding-left: 36px;");
+                    if (i > 0)
+                    {
+                        nextItemLabel->setCursor(Qt::PointingHandCursor);
+                        const api::NewsItem &nextItem = data.items[i - 1];
+                        nextItemLabel->setText(nextItem.title);
+                        connect(nextItemLabel, &ClickableLabel::clicked,
+                                this, [this, nextId = nextItem.id]()
+                        {
+                            emit navigate({Page::NEWS, nextId});
+                        });
+                    }
+                    else
+                    {
+                        nextItemLabel->setEnabled(false);
+                    }
+                    ui->navigationLayout->addWidget(nextItemLabel);
+
+                    ui->contentStackedWidget->setCurrentWidget(ui->contentResultsPage);
+
+                    return;
                 }
-                else if (networkReply->error() != QNetworkReply::OperationCanceledError)
-                {
-                    qDebug() << networkReply->error()
-                             << networkReply->errorString()
-                             << QString(networkReply->readAll()).toUtf8();
-                }
-                networkReply->deleteLater();
-            });
-
-            ui->publishedLabel->setText(QLocale::system().toString(displayedNews.publishDate, QLocale::ShortFormat));
-            ui->descriptionWebEngineView->setHtml(displayedNews.body);
-            ui->seeCommentsButton->setText(ui->seeCommentsButton->text().arg(displayedNews.commentsCount));
-
-            QUrl forumLink(displayedNews.forumThreadLink);
-            connect(ui->seeCommentsButton, &QPushButton::clicked,
-                    this, [this, forumLink]()
-            {
-                QDesktopServices::openUrl(forumLink);
-            });
-            ui->goBackButton->setEnabled(false);
-            ui->goForwardButton->setEnabled(false);
-            ui->contentStackedWidget->setCurrentWidget(ui->contentResultsPage);
+            }
         }
         else if (networkReply->error() != QNetworkReply::OperationCanceledError)
         {
